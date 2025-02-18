d1814de67846f8b5503bf17a5eb5f2a9df723736
0art0 <18333981+0art0@users.noreply.github.com>
2025-02-18 14:04:58 +0000

    Initialize repository

---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «HumanProof» where
+  -- add package configuration options here
+
+lean_lib «HumanProof» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «humanproof» where
+  root := `«HumanProof»
