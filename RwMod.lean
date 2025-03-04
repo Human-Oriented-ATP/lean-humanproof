99a444ef49c4684504bda659cd6ed0629fa11073
Mirek Olšák <mirek@olsak.net>
2025-03-04 16:56:43 +0000

    nicer RwRoot & use locations

--- RwMod.lean
+++ RwMod.lean
@@ -2,19 +2,28 @@ import Mathlib
 
 example (a b : ℕ) : a+b = b+a := by exact Nat.add_comm a b
 
-open Lean Meta Elab Tactic Qq
+open Lean Meta Parser Elab Tactic Qq
 
 namespace ModRw
 
-def Rewriter (n e : Q(ℤ)): Type :=
+def RwRoot : Type :=
+  Expr → Expr → MetaM (Option (Σ _ : Expr, Expr))
+
+/--
+Typed variant of RwRoot in order to let
+Qq check our proof composition
+-/
+def RwRootTyped (n e : Q(ℤ)): Type :=
   MetaM (Option (Σ re : Q(ℤ), Q($e ≡ $re [ZMOD $n])))
+
+example (rwRoot : ∀ n e, RwRootTyped n e) : RwRoot := rwRoot
 
 partial
 def rwInt
     (n : Q(ℤ))
-    (rwRoot : ∀ e, Rewriter n e)
+    (rwRoot : ∀ e, RwRootTyped n e)
     (e : Q(ℤ))
-    : Rewriter n e := do
+    : RwRootTyped n e := do
   match ← rwRoot e with
   | some x => return x
   | none =>
@@ -55,7 +64,7 @@ def rwInt
     | _ => return none
 
 def rwZModEq (n a b : Q(ℤ))
-    (rwRoot : ∀ e, Rewriter n e) :
+    (rwRoot : ∀ e, RwRootTyped n e) :
     MetaM (Option (Σ eq2 : Q(Prop), Q(($a ≡ $b [ZMOD $n]) = $eq2))) := do
   match (← rwInt n rwRoot a, ← rwInt n rwRoot b) with
   | (some ⟨ar, apf⟩, some ⟨br, bpf⟩) =>
@@ -68,14 +77,14 @@ def rwZModEq (n a b : Q(ℤ))
     return none
 
 def rwRootBasic (n₀ a₀ b₀ : Q(ℤ)) (pf : Q($a₀ ≡ $b₀ [ZMOD $n₀]))
-    (n a : Q(ℤ)) : MetaM <| Option  <| Σ b : Q(ℤ), Q($a ≡ $b [ZMOD $n₀])
-    := do
+    : RwRoot :=
+  fun n a => do
     if n == n₀ ∧ a == a₀ then
       return some ⟨b₀, pf⟩
     else
       return none
 
-def rwRootAdvanced (n e : Q(ℤ)) (eqn : Expr) : Rewriter n e := do
+def rwRootAdvanced (eqn : Expr) : RwRoot := fun n e => do
   let thm ← inferType eqn -- the statement that `eqn` proves
   let (mvars, _, body) ← forallMetaTelescope thm -- the body of the statement
   let ⟨1, ~q(Prop), ~q($lhs ≡ $rhs [ZMOD $n])⟩ ← inferTypeQ body | throwError "Expected the equation to be of the form lhs ≡ rhs [ZMOD $mod]"
@@ -111,7 +120,7 @@ def SimpConfig : Simp.Config where
 #print Simp.Simproc
 
 def rwTop
-  (rwRoot : ∀ n e : Q(ℤ), MetaM (Option (Σ re : Q(ℤ), Q($e ≡ $re [ZMOD $n]))))
+  (rwRoot : ∀ n e : Q(ℤ), RwRootTyped n e)
   (tgt : Expr)
   : MetaM Simp.Result := do
   let ctx : Simp.Context ← Simp.mkContext SimpConfig
@@ -120,7 +129,7 @@ def rwTop
   let methods := { pre := rwTopStep rwRoot }
   (·.1) <$> Simp.main tgt ctx (methods := methods)
 
-def rwModTarget (rwRoot : ∀ n e, Rewriter n e) :
+def rwModTarget (rwRoot : ∀ n e, RwRootTyped n e) :
     TacticM Unit := do
   let mvarId ← getMainGoal
   let tgt ← instantiateMVars (← mvarId.getType)
@@ -128,7 +137,7 @@ def rwModTarget (rwRoot : ∀ n e, Rewriter n e) :
   if mvarIdNew == mvarId then throwError "push made no progress"
   replaceMainGoal [mvarIdNew]
 
-def rwModLocalDecl (rwRoot : ∀ n e, Rewriter n e) (fvarId : FVarId) :
+def rwModLocalDecl (rwRoot : ∀ n e, RwRootTyped n e) (fvarId : FVarId) :
     TacticM Unit := do
   let ldecl ← fvarId.getDecl
   if ldecl.isAuxDecl then return
@@ -139,26 +148,20 @@ def rwModLocalDecl (rwRoot : ∀ n e, Rewriter n e) (fvarId : FVarId) :
   if mvarIdNew == mvarId then throwError "push made no progress"
   replaceMainGoal [mvarIdNew]
 
-elab "rw_mod " t:term : tactic =>
+elab "rw_mod " t:term loc?:(location)? : tactic =>
   withMainContext do
+    let loc := match loc? with
+    | some loc => expandLocation loc
+    | none => Location.targets #[] true
     let pf ← elabTerm t none
     let t ← inferType pf
-    match t.app3? `Int.ModEq with
-    | some (n,a,b) =>
-      let rwRoot := rwRootBasic n a b pf
-      rwModTarget rwRoot
+    let rwRoot ← match t.app3? `Int.ModEq with
+    | some (n,a,b) => pure <| (rwRootBasic n a b pf)
     | none => throwError "mod_rw must be provided with a proof of a statement of the form a ≡ b [ZMOD n]"
-
-elab "rw_mod " t:term " at " i:ident : tactic =>
-  withMainContext do
-    let pf ← elabTerm t none
-    let t ← inferType pf
-    match t.app3? `Int.ModEq with
-    | some (n,a,b) =>
-      let rwRoot := rwRootBasic n a b pf
-      let fvarId ← getFVarId i
-      rwModLocalDecl rwRoot fvarId
-    | none => throwError "mod_rw must be provided with a proof of a statement of the form a ≡ b [ZMOD n]"
+    withLocation loc
+      (rwModLocalDecl rwRoot)
+      (rwModTarget rwRoot)
+      (fun _ ↦ logInfo "push_neg couldn't find a negation to push")
 
 example (a b c d n : ℤ) (h : b + c * d ≡ d + b * b [ZMOD n])
 (eq : a ≡ b [ZMOD n])
