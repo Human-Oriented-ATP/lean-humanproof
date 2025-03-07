d711a78ff145324468185db76247be3e8a9655cc
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-03-07 17:45:12 +0000

    set kind to syntheticOpaque

--- HumanProof/Basic.lean
+++ HumanProof/Basic.lean
@@ -235,7 +235,7 @@ where
         return .forallB decl body hidden
   | metaVar mvarId name type body => do
     let type ← type.replaceVars
-    let mvar ← mkFreshExprMVar type (userName := name) (kind := .synthetic)
+    let mvar ← mkFreshExprMVar type (userName := name) (kind := .syntheticOpaque)
     modify (· %.mvarReplacements (·.insert mvarId mvar))
     let mvarId := mvar.mvarId!
     pushGoal mvarId
